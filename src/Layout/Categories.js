1f13f1ff594586a57c554c75b3b2f5aa0996827c
Tony Tran <tran.tony.08@gmail.com>
2022-02-18 19:13:23 -0800

    Completed frontend for Forgot Pass page, need to fix categories page overlay text

--- src/Layout/Categories.js
+++ src/Layout/Categories.js
@@ -1,29 +1,47 @@
 import React from "react";
 import animationStyles from "../Components/animationStyles";
 import { StyleRoot } from "radium";
+import { AnimationWrapper } from "react-hover-animation";
 
 function Categories() {
   return (
     <StyleRoot>
       <div class="container" style={animationStyles.fadeIn}>
         <div class="row">
+          <div class="categories-thumbnails col-4">
+            <AnimationWrapper>
+              <img
+                src={require("../Images/mens-clothing-thumbnail.jpg")}
+                alt="Men's Clothing"
+              />
+              <h2>Men's Clothing</h2>
+            </AnimationWrapper>
+          </div>
+
           <div class="col-4">
-            <h2>Men's Clothing</h2>
+            <AnimationWrapper>
+              <h2>Women's Clothing</h2>
+            </AnimationWrapper>
           </div>
           <div class="col-4">
-            <h2>Women's Clothing</h2>
+            <AnimationWrapper>
+              <h2>Home Furniture</h2>
+            </AnimationWrapper>
           </div>
           <div class="col-4">
-            <h2>Home Furniture</h2>
+            <AnimationWrapper>
+              <h2>Appliances</h2>
+            </AnimationWrapper>
           </div>
           <div class="col-4">
-            <h2>Appliances</h2>
+            <AnimationWrapper>
+              <h2>Electronics</h2>
+            </AnimationWrapper>
           </div>
           <div class="col-4">
-            <h2>Electronics</h2>
-          </div>
-          <div class="col-4">
-            <h2>Groceries</h2>
+            <AnimationWrapper>
+              <h2>Groceries</h2>
+            </AnimationWrapper>
           </div>
         </div>
       </div>
--- src/Layout/ForgotPassword.js
+++ src/Layout/ForgotPassword.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { StyleRoot } from "radium";
 import animationStyles from "../Components/animationStyles";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function ForgotPassword() {
+  const navigate = useNavigate();
+
   return (
     <StyleRoot>
-      <div style={animationStyles.fadeIn}>
-        <p>Forgot Password</p>
+      <div class="container forgot-form-text" style={animationStyles.fadeIn}>
+        <div class="row mt-5 justify-content-center">
+          <h2 class="mb-4">Forgot Password</h2>
+          <form class="col-3">
+            <div>
+              <input
+                class="form-control"
+                type="email"
+                placeholder="E-Mail"
+                aria-label="E-Mail"
+              />
+              <button
+                class="btn btn-success mx-2 mt-3"
+                type="submit"
+                onClick={() => navigate("/")}
+              >
+                Submit
+              </button>
+              <button class="btn btn-warning mx-2 mt-3">Clear</button>
+            </div>
+          </form>
+        </div>
       </div>
     </StyleRoot>
   );
